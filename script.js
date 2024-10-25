d7dee116a6a4bf9eae589d9aa1b4abf7d1d35a8b
Aditya  Nair <148903483+adityacodeslife@users.noreply.github.com>
2024-10-25 11:29:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+let n=0;
+let x=0;
+while (n<5) 
+    {
+    n++;
+    x+n;
+    console.log(x);
+}
+let i = 0; 
+while (i < 5) { 
+  console.log("value " + i); 
+  i++; 
+}
+
+function greet(name) {
+    console.log(`Hello,${name}!`);
+}
+greet ('Aditya');
+function greet2(name) {
+    console.log('Hello', name);
+}
+greet ('tutu');
+
+var str = "hello World";
+var num = 123;
+var bool = true;
+var nullvar = undefined;
+var bigint = 4349529292844858;
+var sym = Symbol("hello dd");
+var obj = {name:"asdf", age: 20};
+var arr = [1,2,3,4,5];
+console.log(str);
+console.log(num);
+console.log(bool);
+console.log(nullvar);
+console.log(bigint);
+console.log(sym);
+console.log(obj);
+console.log(arr); 
+fruits.push("orange")
+console.log (fruits);
